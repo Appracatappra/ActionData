97b3f5bb388b1126984968a4bb8f711e054b4193
Kevin Mullins <kevin_mullins@appracatappra.com>
2017-11-30 13:28:17 -0600

    Initial Setup
    Initially setup library project

---
+++ ActionData/ActionData.h
@@ -0,0 +1,19 @@
+//
+//  ActionData.h
+//  ActionData
+//
+//  Created by Kevin Mullins on 11/30/17.
+//  Copyright © 2017 Appracatappra, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionData.
+FOUNDATION_EXPORT double ActionDataVersionNumber;
+
+//! Project version string for ActionData.
+FOUNDATION_EXPORT const unsigned char ActionDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionData/PublicHeader.h>
+
+
